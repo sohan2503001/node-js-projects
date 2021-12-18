353593f5dbebf50af7cf637374873cdcae5a5796
sohan2503001 <sohanjambhule973@gmail.com>
2021-12-18 21:23:04 +0530

    Setup app for heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,8 +5,10 @@ const express = require('express')
 const hbs = require('hbs');
 const { query } = require('express');
 
-//Define paths for Express congif
 const app = express();
+const port = process.env.PORT || 3000;
+
+//Define paths for Express congif
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
@@ -95,6 +97,6 @@ app.get('*', (req, res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port')
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 })
