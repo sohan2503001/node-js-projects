06055d1178ba8fdff2645b53d2ad1df81609ad1e
sohan2503001 <sohanjambhule973@gmail.com>
2021-12-18 18:26:16 +0530

    Init commit

---
+++ web-server/src/app.js
@@ -0,0 +1,100 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast.js')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs');
+const { query } = require('express');
+
+//Define paths for Express congif
+const app = express();
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(path.join(__dirname, '../public/')))
+
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Sohan Jambhule'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Sohan Jambhule'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help ME',
+        name: 'Sohan jambhule',
+        message: 'Hey Sohan please help me i about to die ! '
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide address'
+        })
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastdata) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            res.send({
+                    forecast: forecastdata,
+                    location,
+                    address: req.query.address,
+                })
+        })
+    })
+})
+
+app.get('/products', (req, res) => {
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search'
+        })
+    }
+    res.send({
+        product: []
+    })
+})
+
+app.get('/help/*', (req,res) => {
+    res.render('404', {
+        title: '404',
+        name: 'sohan jambhule',
+        errormessage: 'help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '4040',
+        name: 'Sohan Jambhule',
+        errormessage: 'Page not found'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port')
+})
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=733d154e5afc9b22ac319f3074fba99c&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + ''
+
+    request({
+        url,
+        json: true,
+    }, (error, {body} = {}) => {
+        if (error) {
+            callback("Unable to connect to weather service", undefined);
+        } else if (body.error) {
+            callback("Unable to find location", undefined);
+        } else {
+            callback(
+                undefined, `${body.current.weather_descriptions[0]} It is currently ${body.current.temperature} degree out. But it feels like ${body.current.feelslike}`
+            );
+        }
+    })
+}
+
+module.exports = forecast
